7dbc66e2dd00d26bedecf887d55441bc5905ca43
Nimrod S. Kerrett <dod@goleshet.com>
2011-10-08 03:38:17 +0700

    Initial commit

---
+++ evently/account/browserID/after.js
@@ -0,0 +1,30 @@
+function() {
+  var elem=$(this);
+  navigator.id.getVerifiedEmail(function(assertion) {
+    if (assertion) {
+      // Verify through CouchDB.
+      var url = '/_browserid';
+      var to_verify = { 'assertion': window.encodeURIComponent(assertion)
+                      , 'audience' : window.encodeURIComponent(window.location.host)
+                      };
+  
+      $.ajax({
+        url: url,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(to_verify),
+        dataType: "json",
+        success: function(data, textStatus, jqXHR) {
+          $('#account').trigger('_init'); // Now we're logged in
+        },
+  
+        error: function(jqXHR, textStatus, errorThrown) {
+          alert('error: '+textStatus);
+        }
+      });
+    } else {
+      // something went wrong!  the user isn't logged in.
+      alert("BrowserID login failed");
+    }
+  });
+}
