90e86a03edab51b8fa74fea883372ef9919c77ce
Nimrod S. Kerrett <dod@goleshet.com>
2011-10-11 05:13:38 +0700

    Added logo, favicon and rewrites. Removed yahoo player. Updated README.
    Sorry about the player, but fixing the bugs required a massive rewrite
    (and it wasn't THAT important). Also - it wasn't very wise to provide
    info to yahoo from a privacy-minded app (doh).

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -6,7 +6,6 @@ function(data) {
           m = (r.value && r.value.message) || '';
       // the replace() tweak is ugly, but twttr doesn't support {target:"_blank"} for @name, #tag, etc.
       p.message = twttr.txt.autoLink(twttr.txt.htmlEscape(m)).replace(/<a /g,'<a target="_blank" ');
-      p.id = 'm' + ((r.value && r.value._id) || new Date().getTime().toString());
       return p;
     })
   }
