9c0808d1dbf7f958de0b94117ceab90d21ea5360
thedod <unclezzzen@gmail.com>
2011-11-05 18:06:11 +0700

    Defusing javascript: url in profile
    Thanks for the heads up, Galebo

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -4,6 +4,9 @@ function(data) {
     items : data.rows.map(function(r) {
       var p = (r.value && r.value.profile) || {},
           m = (r.value && r.value.message) || '';
+      if (p.url && !p.url.match(/^https?:\/\//i)) {
+          delete p.url; // beware the javascript: my son
+      }
       // the replace() tweak is ugly, but twttr doesn't support {target:"_blank"} for @name, #tag, etc.
       p.message = twttr.txt.autoLink(twttr.txt.htmlEscape(m)).replace(/<a /g,'<a target="_blank" ');
       p.id = 'm' + (r.value && r.value._id || new Date().getTime().toString());
