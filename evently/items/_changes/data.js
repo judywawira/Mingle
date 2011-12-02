584d6958e2d2940561254879cff173c5f931b2e4
thedod <unclezzzen@gmail.com>
2011-12-02 18:57:33 +0700

    Larger avatar in pop up
    Plain vanity

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -7,6 +7,9 @@ function(data) {
       if (p.url && !p.url.match(/^https?:\/\//i)) {
           delete p.url; // beware the javascript: my son
       }
+      if (p.gravatar_url) {
+          p.gravatar_url_large = p.gravatar_url.replace('s=40&','s=100&');
+      }
       // the replace() tweak is ugly, but twttr doesn't support {target:"_blank"} for @name, #tag, etc.
       p.message = twttr.txt.autoLink(twttr.txt.htmlEscape(m)).replace(/<a /g,'<a target="_blank" ');
       p.id = 'm' + (r.value && r.value._id || new Date().getTime().toString());
